e7cd54c1abfaf99f8e23603a786f769300875720
Dean Merchant <deanmerchant@gmail.com>
2017-05-18 19:56:36 +0100

    Made a cli to lookup words from the oxford dictionary api

---
+++ README.md
@@ -0,0 +1,15 @@
+# define
+
+A command line utility to retrieve word definitions
+
+How to use
+- `stack build` 
+- copy the built executable from .stack-work/install/<your-platform-info>/bin to somewhere on your path
+- sign up for a free api key at https://developer.oxforddictionaries.com
+- put the app id and api key in a `.define.yaml` file in your home directory like so
+
+ ```
+ apiKey: somestringoflettersandnumberswhichisquitelong
+ appId: ashorterstirngofnumbersandletters
+ ```
+
---
+++ src/Main.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Applicative
+import Control.Lens
+import Data.Aeson
+import Data.Aeson.Lens (_String, key, values)
+import qualified Data.Char as Char
+import Data.Text hiding (take)
+import Data.Text.Encoding (encodeUtf8)
+import Data.Yaml
+import Network.HTTP.Client hiding (checkResponse, responseBody, responseStatus)
+import Network.HTTP.Types.Status (Status(..), status403, status404)
+import Network.Wreq
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.String
+import System.Directory (getHomeDirectory)
+import System.Environment (getArgs)
+import System.FilePath (joinPath)
+
+apiBaseUrl = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/"
+language = "en"
+
+toWordId :: String -> String
+toWordId = fmap Char.toLower
+
+getOptions :: AppConfig -> Options
+getOptions appConfig = defaults
+  & set (header "accept") ["application/json"]
+  & set (header "app_key") [encodeUtf8 (apiKey appConfig)]
+  & set (header "app_id") [encodeUtf8 (appId appConfig)]
+  & set checkResponse (Just responseChecker)
+    where
+      -- Prevents an exception being thrown on a non 'ok' status
+      responseChecker :: Request -> (Response a) -> IO ()
+      responseChecker req res = return ()
+
+urlForWordDefinitions :: String -> String
+urlForWordDefinitions word = apiBaseUrl ++ (toWordId word) ++ "/definitions"
+
+getWordDefinition :: Options -> String -> IO (Status, Maybe Text)
+getWordDefinition opts word = do
+  response <- getWith opts (urlForWordDefinitions word)
+  let status = response ^. responseStatus
+  let wordDef = response ^? responseBody
+        . key "results" . values
+        . key "lexicalEntries" . values
+        . key "entries" . values
+        . key "senses" . values
+        . key "definitions" . values
+        . _String
+  return $ (status, wordDef)
+
+data AppConfig = AppConfig { apiKey :: Text, appId :: Text } deriving Show
+instance FromJSON AppConfig where
+  parseJSON (Object m) = AppConfig <$> m .: "apiKey" <*> m .: "appId"
+  parseJSON x = fail $ show x
+
+configFilePath :: IO FilePath
+configFilePath = getHomeDirectory >>=
+  \homeDir -> return $ joinPath [homeDir, ".define.yaml"]
+
+readConfigFile :: IO (Either ParseException AppConfig)
+readConfigFile =
+  decodeFileEither =<< configFilePath
+
+main :: IO ()
+main = do
+  words <- fmap (take 1) getArgs
+  case words of
+    [] -> print "usage: define <word>"
+    word:_ -> do
+      config <- readConfigFile
+      case config of
+        Left (InvalidYaml (Just (YamlException msg))) -> print $ "Missing config gile" ++ msg
+        Left (InvalidYaml (Just (YamlParseException _ _ (YamlMark _ line column)))) -> print $ "Invalid config file, check your formatting at line: " ++ show line ++ " column: " ++ show column
+        Left (AesonException msg) -> print $ "Invalid config gile " ++ msg
+        Left x -> print $ show x
+        Right appConfig -> do
+          definitionResponse <- getWordDefinition (getOptions appConfig) word
+          case definitionResponse of
+            (Status 200 _, Just definition) ->  print definition
+            (Status 403 _, _) ->  print "Bad credentials, check your .define file"
+            (Status 404 _, _) ->  print "No definition found"
+            otherwise ->  print definitionResponse
